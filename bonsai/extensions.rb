140ba5042be357651a790b9c49f1339e2684462b
Jakub Pastuszek <jpastuszek@gmail.com>
2012-05-09 22:42:55 +0200

    hacks around bonsai

--- bonsai/extensions.rb
+++ bonsai/extensions.rb
@@ -1,13 +1,32 @@
 module Bonsai
+	class Exporter
+		class << self
+			def write_pages
+				Bonsai.log "Writing pages"
+				Page.all.select{|page| page.permalink != '/index/'}.each do |page|
+					Bonsai.log "\t Writing page - #{page.permalink}"
+					FileUtils.mkdir_p("#{path}#{page.permalink}")
+					File.open("#{path}#{page.write_path}", "w"){|file| file.write(page.render) }
+				end
+			end
+		end
+	end
+
 	class Page
 		def relative_root
-			p Page.all
+			return '' if permalink == '/index/'
 			permalink.split('/').select{|a| not a.empty?}.map{|a| '..'}.join('/') + '/'
 		end
 
 		alias :orig_to_hash :to_hash
 		def to_hash
-			p orig_to_hash.merge('relative_root' => relative_root)
+			h = orig_to_hash.merge('relative_root' => relative_root)
+			p permalink
+			if permalink == '/index/'
+				h['permalink'] = '/'
+				h['slug'] = ''
+			end
+			p h
 		end
 	end
 end
